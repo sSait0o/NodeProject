39ef7b99786a57fe6c9dd431d82dbfe03585140f
William Lin <linwilliam14@gmail.com>
2025-02-20 11:27:17 +0100

    Suppressions de books

--- api/index.js
+++ api/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
-const app = express(); // Instanciation d'Express déplacée ici
-const Book = require("./models/book");
+const app = express();
 const User = require("./models/user");
 require("dotenv").config();
 const bcrypt = require("bcrypt");
@@ -11,71 +10,6 @@ app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.json());
 
 const SECRET_KEY = process.env.SECRET_KEY;
-
-// Middleware de protection des routes
-const authenticate = (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1];
-  if (!token) return res.status(401).json({ error: "Accès non autorisé" });
-  try {
-    const decoded = jwt.verify(token, SECRET_KEY);
-    req.user = decoded;
-    next();
-  } catch (error) {
-    res.status(403).json({ error: "Token invalide" });
-  }
-};
-
-// Endpoints pour les livres
-app.get("/books", authenticate, async (req, res) => {
-  try {
-    const books = await Book.getAllBooks();
-    res.status(200).json(books);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-// GET SPECIFIC
-app.get("/books/:id", authenticate, async (req, res) => {
-  try {
-    const book = await Book.getBookById(req.params.id);
-    book
-      ? res.status(200).json(book)
-      : res.status(404).json({ message: "Pas trouvé" });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-// POST BOOK
-app.post("/books", authenticate, async (req, res) => {
-  try {
-    const newBook = await Book.createBook(req.body);
-    res.status(201).json(newBook);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-// PUT BOOK
-app.put("/books/:id", authenticate, async (req, res) => {
-  try {
-    const updatedBook = await Book.updateBook(req.params.id, req.body);
-    res.status(200).json(updatedBook);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-// DELETE BOOK
-app.delete("/books/:id", authenticate, async (req, res) => {
-  try {
-    await Book.deleteBook(req.params.id);
-    res.status(204).send();
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
 
 // ROUTE : Inscription
 app.post("/register", async (req, res) => {
